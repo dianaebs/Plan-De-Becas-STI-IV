b8726af6cd9b1acd9d0322742555d6f5bc5f9c33
diana eloisa bautista suarez <elisartemis_mond@live.com.mx>
2018-10-09 14:09:04 -0700

    First commit

---
+++ program-C/Semana1/archivo1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (){
+int Numero;
+Numero  = 1;
+
+
+printf("hola diana\n");
+printf("hola dIana\t");
+printf("\nMi numero es %d",Numero);
+Numero = 3;
+printf("\nMinumero es %d",Numero);
+
+
+} 
